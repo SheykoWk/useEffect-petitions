73e9d0cd7cc0785cfd25bf14e927dbd7db68cca0
SheykoWk <sheyko.wk@gmail.com>
2022-03-28 21:57:26 -0600

    Map method

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,13 +3,31 @@ import Abilities from "./Abilities"
 const Pokemon = ({name, image, id, abilities}) => {
 
     const abilitieList = abilities.map((ability) => <Abilities name={ability.ability.name} key={ability.ability.name} />)
-    console.log(abilities)
+
+    const frutas = ['manzana', 'pera', 'sandia', 'papaya', 'fresa']
+
+    const nums = [1 , 7, 5, 9]
+
+    const numList = nums.map((num) => num + 10)
+    console.log(numList)
+    console.log(nums)
+
+    const arrList = frutas.map((fruta) => `Amo la ${fruta}`)
+
+    console.log(arrList)
+    console.log(frutas)
+
+    for(let fruta of frutas){
+        console.log(`Amo la ${fruta}`)
+    }
+
     return(
         <div>
             <h1>{name}</h1>
             <h3> {id} </h3>
             <img src={image} alt='' />
             {abilitieList}
+            {arrList}
         </div>
     )
 }
