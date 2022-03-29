b85f2097d770478749400df7c2819cd6f6d6bf62
SheykoWk <sheyko.wk@gmail.com>
2022-03-28 19:47:06 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+  const [pokemon, setPokemon] = useState('pikachu')
+ 
+  useEffect(() => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+
+    axios.get(url)
+    .then(res => console.log(res))
+    .catch(error => console.log(error));
+
+
+  }, [pokemon])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setPokemon('charizard')}>Change to Charizard</button>
       </header>
     </div>
   );
